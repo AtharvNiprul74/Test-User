b37b33b9fcfe7c767488f51b420326fb605361ba
AtharvNiprul74 <atharvniprul74@gmail.com>
2026-01-07 22:56:51 +0530

    Done

--- Database/db.js
+++ Database/db.js
@@ -1,4 +1,13 @@
 const mysql2 = require("mysql2")
+
+console.log(
+    process.env.MYSQLHOST,
+    process.env.MYSQLUSER,
+    process.env.MYSQLPASSWORD,
+    process.env.MYSQLDATABASE,
+    process.env.MYSQLPORT
+)
+
 const pool = mysql2.createPool({
     host:process.env.MYSQLHOST,
     user:process.env.MYSQLUSER,
